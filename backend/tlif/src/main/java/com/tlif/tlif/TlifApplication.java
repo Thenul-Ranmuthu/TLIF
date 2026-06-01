f79dec8e97b5085d38e4b336f9839b90c40156d6
Thenul-Ranmuthu <thenul.ranmuthu@gmail.com>
2026-06-01 19:31:14 +0530

    Initial commit

---
+++ backend/tlif/src/main/java/com/tlif/tlif/TlifApplication.java
@@ -0,0 +1,13 @@
+package com.tlif.tlif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TlifApplication.class, args);
+	}
+
+}
---
+++ backend/tlif/src/test/java/com/tlif/tlif/TlifApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlif.tlif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TlifApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
